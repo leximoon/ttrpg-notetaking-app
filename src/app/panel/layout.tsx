8d1fa8b4ec562503cbf5053e9f5379b461b13e05
Alex Luna <alexlunabares@gmail.com>
2024-11-12 17:59:33 +0100

    Added responsive resizing

--- src/app/panel/layout.tsx
+++ src/app/panel/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 const AppLayout = ({ children }: { children: ReactNode }) => {
     return (
         <div className="bg-background w-screen h-screen flex justify-center items-center">
-            <div className="flex flex-row rounded-lg border border-gray-300 w-3/6 h-3/5  shadow-md shadow-shadowColor">
+            <div className="flex flex-row rounded-lg border border-gray-300 w-5/6 h-4/5 xl:w-3/6 xl:h-3/5  shadow-md shadow-shadowColor">
                 <aside className="z-40 w-56 rounded-l-lg bg-card border-r-2 border-gray-300 p-5 pt-10 pl-7 text-sm">
                     <div className="h-full flex flex-col justify-between">
                         <NavList />
