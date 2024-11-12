001eaf3b14f46c5480ae0f5966638306b484e0dd
Alex Luna <alexlunabares@gmail.com>
2024-11-12 17:45:52 +0100

    Added provisional spinner component to wait while loading components from server Mino change in listelement

--- src/app/panel/_components/listElement/listElement.tsx
+++ src/app/panel/_components/listElement/listElement.tsx
@@ -13,7 +13,7 @@ const ListElement = ({
 }: ListElementProps) => {
     return (
         <>
-            <li className="flex items-center border py-2 my-1 cursor-pointer hover:bg-secondary/15">
+            <li className="flex items-center border py-2 my-3 cursor-pointer hover:bg-secondary/15">
                 <div className="mx-4 text-text">{icon}</div>
                 <div className="flex flex-col font-semibold">
                     <span className="text-sm text-text">{name}</span>
---
+++ src/components/UI/spinner/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="animate-spin rounded-full h-6 w-6 border-t-4  border-l-4 border-primary"></div>
+        </div>
+    );
+};
+
+export { Spinner };
