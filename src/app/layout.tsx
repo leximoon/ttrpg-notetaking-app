96cf7a3f4e8d3f93df7f3b9c8f84dba8f70daed1
Alex Luna <alexlunabares@gmail.com>
2024-11-12 17:44:01 +0100

    added theme provider to the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "./provider";
 
 export const metadata: Metadata = {
-  title: "TTRPG Notetaking App",
-  description: "Web app for taking notes for TTRPGs with prefabricated templates",
+    title: "TTRPG Notetaking App",
+    description:
+        "Web app for taking notes for TTRPGs with prefabricated templates",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="light" suppressHydrationWarning>
+            <body>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    );
 }
