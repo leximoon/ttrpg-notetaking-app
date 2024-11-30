496009d65f515b1cd4028643aa566b40fc7d157e
Alex Luna <alexlunabares@gmail.com>
2024-11-30 21:32:18 +0100

    Minor aesthetic modifications

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,12 +2,14 @@ import { Footer } from "./_components/footer";
 import Header from "./_components/header";
 import { Main } from "./_components/main";
 
-export default function () {
+export default function LandingPage() {
     return (
         <>
             <div className="min-h-full flex flex-col bg-background">
-                <div className="flex flex-col items-center justify-center
-                md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+                <div
+                    className="flex flex-col items-center justify-center
+                md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"
+                >
                     <Header />
                     <Main />
                     <Footer />
