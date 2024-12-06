31fe5bee7bd052b20f451f960375c937b2b8b183
leximoon <alexlunabares@gmail.com>
2024-12-06 21:49:36 +0100

    changed button variant to transparent

--- src/app/landing/_components/footer.tsx
+++ src/app/landing/_components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
 		<div className="flex items-center w-full p-6 bg-background z-50">
 			<Logo />
 			<div className=" w-full justify-end flex items-center gap-x-2 text-text">
-				<Button /* TODO: Change to a transparent option once created */ intent={"primary"} variant={"fill"} size={"m"} label="Contact Us" />
+				<Button intent={"primary"} variant={"transparent"} size={"m"} label="Contact Us" />
 			</div>
 		</div>
 	);
