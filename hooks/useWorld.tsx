94c0ecbdcddac2794e0f71f0b290a6aa9621aec0
Alex Luna <alexlunabares@gmail.com>
2024-12-08 19:15:31 +0100

    added functionality to store the current world id into the hook and retrive the world from database

--- hooks/useWorld.tsx
+++ hooks/useWorld.tsx
@@ -1,17 +1,28 @@
-import { createWorld } from "@/lib/api/worldsApi";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createWorld, getWorldById } from "@/lib/api/worldsApi";
+import { World } from "@/types/world";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 
-export function useWorldsMutations() {
+export function useWorlds(worldId: string) {
     const queryClient = useQueryClient();
+    const [currentWorldId, setCurrentWorld] = useState<string>(worldId);
 
     const addWorld = useMutation({
         mutationFn: createWorld,
-        onSuccess: () => {
+        onSuccess: ({ id }) => {
+            setCurrentWorld(id);
             queryClient.invalidateQueries({ queryKey: ["worlds"] });
         },
     });
 
+    const useCurrentWorld = () =>
+        useQuery<World | null, Error>({
+            queryKey: ["world", currentWorldId],
+            queryFn: () => getWorldById(currentWorldId),
+        });
+
     return {
         addWorld,
+        useCurrentWorld,
     };
 }
