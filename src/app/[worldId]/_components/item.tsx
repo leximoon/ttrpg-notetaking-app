9191fa8a317ef13fcf76173f4f098e0c9228c7ee
leximoon <alexlunabares@gmail.com>
2025-01-19 13:10:26 +0100

    added handleExpand, onCreate for document items

--- src/app/[worldId]/_components/item.tsx
+++ src/app/[worldId]/_components/item.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
+import { Skeleton } from "@nextui-org/skeleton";
+import { useDocument } from "@/hooks/useDocument";
+import { usePathname } from "next/navigation";
 
 interface ItemProps {
     id?: string;
@@ -28,9 +31,31 @@ export const Item = ({
     onExpand,
     ...props
 }: ItemProps) => {
+    const handleExpand = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        onExpand?.();
+    };
+
+    const pathname = usePathname();
+    const worldId = pathname.split("/")[1]; //get first element from url which is worldId
+
+    // CREATE NEW DOCUMENT
+    const { addDocument } = useDocument();
+    const onCreate = ( 
+        e: React.MouseEvent<HTMLDivElement,MouseEvent>
+    ) => {
+        if(!id) return
+        addDocument.mutate(
+            {title:"Untitled", worldId: worldId, parentDocumentId: id}
+        );
+    }
+    // DROPDOWN ICON >
     const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
     return (
-        <div
+        <div // BASIC ITEM 
             onClick={onClick}
             role="button"
             style={{
@@ -39,32 +64,55 @@ export const Item = ({
             className={twMerge(
                 clsx(
                     "group min-h-[27px] test-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-primary-muted font-medium",
-                    {"bg-primary/5 text-primary": active},
+                    { "bg-primary/5 text-primary": active }
                 )
             )}
         >
-            {!!id && (
+            {!!id && ( // DROPDOWN BUTTON TODO: Change for only if children 
                 <div
-                role="button"
-                className="h-full rounded-sm hover:bg-accent mr-1"
-                onClick={() => {}}
+                    role="button"
+                    className="h-full rounded-sm hover:bg-accent mr-1"
+                    onClick={handleExpand}
                 >
-                    <ChevronIcon
-                    className="h-4 w-4 shrink-0 text-primary-muted/50"
-                    />
+                    <ChevronIcon className="h-4 w-4 shrink-0 text-primary-muted/50" />
                 </div>
             )}
-            <Icon className="shrink-0 h-[18px] mr-2 text-primary-muted" />
-            <span className="truncate">
-                {label}
-            </span>
-            {isSearch && (
-                <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 
+            <Icon //DOCUMENT ICON
+            className="shrink-0 h-[18px] mr-2 text-primary-muted" />
+            <span className="truncate">{label}</span>
+            {isSearch && ( //SEARCH BAR ITEM
+                <kbd
+                    className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 
                 rounded border bg-secondary-muted px-1.5 font-mono text-[10px] font-medium text-primary-muted 
-                opacity-100">
+                opacity-100"
+                >
                     <span className="text-xxs">CTRL</span>K
                 </kbd>
             )}
+            {!!id && ( //CREATE BUTTON FOR DOCUMENT ITEMS
+                <div className="ml-auto flex items-center gap-x-2">
+                    <div
+                        role="button"
+                        onClick={onCreate}
+                        className="opacity-0 group-hover:opacity-100 h-full ml-auto
+                    rounded-sm hover:bg-secondary/200 "
+                    >
+                        <Plus className="h-4 w-4 text-primary-muted" />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+    return (
+        <div
+            style={{
+                paddingLeft: level ? `${level * 12 + 25}px` : "12px",
+            }}
+        >
+            <Skeleton className="flex gap-x-2 py-[3px]" />
         </div>
     );
 };
