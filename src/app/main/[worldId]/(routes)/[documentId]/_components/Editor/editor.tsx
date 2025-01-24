db10d6a612e98ee713568cefba07f2263826228f
Alex Luna <alexlunabares@gmail.com>
2025-01-24 01:49:27 +0100

    Added toolbar (header) component Added editor component usint BlockEditor lib

---
+++ src/app/main/[worldId]/(routes)/[documentId]/_components/Editor/editor.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import type { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useTheme } from "next-themes";
+
+interface EditorProps {
+    onChange: (value: string) => void;
+    initialContent?: string;
+    editable?: boolean;
+}
+
+export const Editor = ({
+    onChange,
+    initialContent,
+    editable = true,
+}: EditorProps) => {
+    const { resolvedTheme } = useTheme();
+
+    console.log(initialContent);
+    const editor: BlockNoteEditor | null = useCreateBlockNote({
+        initialContent: initialContent
+            ? (JSON.parse(initialContent) as PartialBlock[])
+            : undefined,
+    });
+
+    return (
+        <div className="">
+            {editor && (
+                <BlockNoteView
+                    editor={editor}
+                    theme={resolvedTheme === "dark" ? "dark" : "light"}
+                    editable={editable}
+                    onChange={() => {
+                        onChange(JSON.stringify(editor.document));
+                    }}
+                />
+            )}
+        </div>
+    );
+};
