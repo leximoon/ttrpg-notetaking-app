d1044623c1293547ff457d45c3f940ada79232af
Alex Luna <alexlunabares@gmail.com>
2025-01-24 17:24:15 +0100

    Added new component EditableField to the metadata

--- src/app/main/[worldId]/(routes)/[documentId]/_components/Metadata/metadata.tsx
+++ src/app/main/[worldId]/(routes)/[documentId]/_components/Metadata/metadata.tsx
@@ -1,27 +1,21 @@
+import { Button } from "@/components/UI/button";
 import React from "react";
+import { EditableField } from "./_components/editableField";
 
 export const Metadata = () => {
     return (
         <div className="flex flex-col">
             <span className="mb-4 font-bold text-2xl">METADATA</span>
-            <span className="m-2">Some text</span>
-            <span className="m-2">
-                Some description super super ultra mega super ultra long that
-                don't fit here nor anywhere
-            </span>
-            <ol className="m-2">
-                <li> # More things</li>
-                <li> # More things</li>
-                <li> # More things</li>
-                <li> # More things</li>
-            </ol>
-            <hr className="m-2" />
-            <ol className="m-2">
-                <li> # More things</li>
-                <li> # More things</li>
-                <li> # More things</li>
-                <li> # More things</li>
-            </ol>
+
+            <div className="grid mb-5">
+                <EditableField variant="title" initialData="Some text" />
+                <EditableField initialData="Some text" />
+            </div>
+            <div className="grid mb-5">
+                <EditableField variant="title" initialData="Some text" />
+                <EditableField initialData="Some text" />
+            </div>
+            {/*<Button size="s" label="Add metadata" /> */}
         </div>
     );
 };
