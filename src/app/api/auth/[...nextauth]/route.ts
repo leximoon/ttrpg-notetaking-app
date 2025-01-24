6c24ab77d310b5f5024209d741194df815d15fae
leximoon <alexlunabares@gmail.com>
2025-01-24 02:43:27 +0100

    deleted console.log message

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,106 +1,102 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { JWT } from "next-auth/jwt";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 async function refreshToken(token: JWT): Promise<JWT> {
-    console.log("RefreshToken: ", token.backendTokens.refreshToken);
-    const res = await fetch(process.env.BACKEND_URL + "/user/refresh", {
-        method: "POST",
-        headers: {
-            authorization: `bearer ${token.backendTokens.refreshToken}`,
-        },
-    });
-    console.log("refreshed");
+	console.log('RefreshToken: ', token.backendTokens.refreshToken);
+	const res = await fetch(process.env.BACKEND_URL + '/user/refresh', {
+		method: 'POST',
+		headers: {
+			authorization: `bearer ${token.backendTokens.refreshToken}`,
+		},
+	});
+	console.log('refreshed');
 
-    const response = await res.json();
-    return {
-        ...token,
-        backendTokens: response,
-    };
+	const response = await res.json();
+	return {
+		...token,
+		backendTokens: response,
+	};
 }
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
 
-            credentials: {
-                username: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                //credentials passed when login, username is just the var name, but can be the email.
-                const { username, password } = credentials as {
-                    username: string;
-                    password: string;
-                };
+			credentials: {
+				username: { label: 'Email', type: 'text' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				//credentials passed when login, username is just the var name, but can be the email.
+				const { username, password } = credentials as {
+					username: string;
+					password: string;
+				};
 
-                //ApiCall to
-                console.log("credentials", { username, password });
-                const res = await fetch("http://localhost:5000/user/login", {
-                    method: "POST",
-                    body: JSON.stringify({ username, password }),
-                    headers: { "Content-Type": "application/json" },
-                });
+				//ApiCall to
+				console.log('credentials', { username, password });
+				const res = await fetch('http://localhost:5000/user/login', {
+					method: 'POST',
+					body: JSON.stringify({ username, password }),
+					headers: { 'Content-Type': 'application/json' },
+				});
 
-                const user = await res.json();
+				const user = await res.json();
 
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user;
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null;
+				if (user) {
+					// Any object returned will be saved in `user` property of the JWT
+					return user;
+				} else {
+					// If you return null then an error will be displayed advising the user to check their details.
+					return null;
 
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                }
-            },
-        }),
-    ],
-    callbacks: {
-        //Function called then nextAuth generate jwt. This will be called in logIn and when the session is checked
-        async jwt({ token, user }) {
-            //Check if the jwt callback is called in the login (user included) or not.
+					// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+				}
+			},
+		}),
+	],
+	callbacks: {
+		//Function called then nextAuth generate jwt. This will be called in logIn and when the session is checked
+		async jwt({ token, user }) {
+			//Check if the jwt callback is called in the login (user included) or not.
 
-            if (user) return { ...token, ...user };
+			if (user) return { ...token, ...user };
 
-            //Checking expiring time
-            if (new Date().getTime() < token.backendTokens.expiresIn) {
-                console.log(
-                    "Token refreshed: ",
-                    token.backendTokens.accessToken
-                );
-                return token;
-            }
+			//Checking expiring time
+			if (new Date().getTime() < token.backendTokens.expiresIn) {
+				return token;
+			}
 
-            //Refreshing access token
-            const refreshedToken = await refreshToken(token);
-            console.log("Token refreshed: ", refreshedToken);
-            return refreshedToken;
-        },
+			//Refreshing access token
+			const refreshedToken = await refreshToken(token);
+			console.log('Token refreshed: ', refreshedToken);
+			return refreshedToken;
+		},
 
-        //Function called when the session is checked
-        async session({ token, session }) {
-            session.user = token.user;
-            return session;
-        },
-    },
-    pages: {
-        signIn: "/auth/login",
-    },
+		//Function called when the session is checked
+		async session({ token, session }) {
+			session.user = token.user;
+			return session;
+		},
+	},
+	pages: {
+		signIn: '/auth/login',
+	},
 };
 
 export const authServerOptions: NextAuthOptions = {
-    ...authOptions,
-    callbacks: {
-        ...authOptions.callbacks,
-        async session({ token, session }) {
-            session.user = token.user;
-            session.backendTokens = token.backendTokens;
-            return session;
-        },
-    },
+	...authOptions,
+	callbacks: {
+		...authOptions.callbacks,
+		async session({ token, session }) {
+			session.user = token.user;
+			session.backendTokens = token.backendTokens;
+			return session;
+		},
+	},
 };
 
 const handler = NextAuth(authOptions);
