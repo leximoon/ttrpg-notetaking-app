0236edb5a2a639db14133810a96b548d2f39fbc6
Alex Luna <alexlunabares@gmail.com>
2025-01-26 03:57:44 +0100

    minor fixes to the button

--- src/components/UI/button/button.tsx
+++ src/components/UI/button/button.tsx
@@ -1,77 +1,77 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-const button = cva(
-    "button rounded-lg text-primary-contrast flex items-center gap-1",
-    {
-        //Properties of the button
+const button = cva("button text-text flex items-center gap-1", {
+    //Properties of the button
 
-        //TODO: Add transparent with no border option
-        variants: {
-            //Defines the main color of the button
-            intent: {
-                primary: [""],
-                secondary: [""],
-                sidebaritem: [""],
-                danger: ["text-text"],
-            },
-            //Define the style of the button
-            variant: {
-                fill: ["border-transparent"],
-                dashed: ["text-text font-semibold border-2 border-dashed"],
-                transparent: ["border-transparent bg-transparent"],
-            },
-            //Define the size of the button
-            size: {
-                s: ["text-sm", "py-1", "px-2"],
-                m: ["text-base", "py-2", "px-4"],
-                l: ["text-lg", "py-3", "px-6"],
-            },
+    //TODO: Add transparent with no border option
+    variants: {
+        //Defines the main color of the button
+        intent: {
+            primary: [""],
+            secondary: [""],
+            sidebaritem: [""],
+            danger: ["text-text"],
         },
-        /**
-         * Composed css classes for correct color selection depending the variant
-         */
-        compoundVariants: [
-            {
-                intent: "primary",
-                variant: "fill",
-                class: "bg-primary hover:bg-primary/70",
-            },
-            {
-                intent: "secondary",
-                variant: "fill",
-                class: "bg-secondary hover:bg-secondary/70",
-            },
-            {
-                intent: "primary",
-                variant: "dashed",
-                class: "border-primary hover:bg-primary/30",
-            },
-            {
-                intent: "secondary",
-                variant: "dashed",
-                class: "border-secondary hover:bg-secondary/30",
-            },
-            {
-                intent: "danger",
-                variant: "fill",
-                class: "bg-danger/70 hover:bg-danger",
-            },
-            {
-                intent: "danger",
-                variant: "transparent",
-                class: "hover:bg-danger/40",
-            },
-        ],
-        /**
-         * Default values
-         *  */
-        defaultVariants: {
+        //Define the style of the button
+        variant: {
+            fill: ["border-transparent rounded-lg"],
+            dashed: [
+                "text-text font-semibold border-2 border-dashed rounded-lg",
+            ],
+            transparent: ["border-transparent bg-transparent"],
+        },
+        //Define the size of the button
+        size: {
+            s: ["text-sm", "py-1", "px-2"],
+            m: ["text-base", "py-2", "px-4"],
+            l: ["text-lg", "py-3", "px-6"],
+            auto: [""],
+        },
+    },
+    /**
+     * Composed css classes for correct color selection depending the variant
+     */
+    compoundVariants: [
+        {
             intent: "primary",
-            size: "m",
             variant: "fill",
+            class: "bg-primary hover:bg-primary/70",
         },
-    }
-);
+        {
+            intent: "secondary",
+            variant: "fill",
+            class: "bg-secondary hover:bg-secondary/70",
+        },
+        {
+            intent: "primary",
+            variant: "dashed",
+            class: "border-primary hover:bg-primary/30",
+        },
+        {
+            intent: "secondary",
+            variant: "dashed",
+            class: "border-secondary hover:bg-secondary/30",
+        },
+        {
+            intent: "danger",
+            variant: "fill",
+            class: "bg-danger/70 hover:bg-danger",
+        },
+        {
+            intent: "danger",
+            variant: "transparent",
+            class: "hover:bg-danger/40",
+        },
+    ],
+    /**
+     * Default values
+     *  */
+    defaultVariants: {
+        intent: "primary",
+        size: "m",
+        variant: "fill",
+    },
+});
 
 //TODO: create role prop for the button
 interface ButtonProps
